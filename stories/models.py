f7ad2b5381cd3afe6725b157639f3a589fbfefa0
shamshodbekkhatamov@gmail.com <shamshodbekkhatamov@gmail.com>
2025-12-15 17:08:09 +0500

    Fix stories image upload for Vercel

--- stories/models.py
+++ stories/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timesince import timesince
 
+# 1. MUHIM IMPORT (Buni qo'shdik)
+from cloudinary_storage.storage import MediaCloudinaryStorage
+
 
 class Story(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stories')
@@ -12,7 +15,8 @@ class Story(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     share_uuid = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    # Layk va Saqlanganlar (Many-to-Many - chunki ko'p user ko'p layk bosishi mumkin)
+
+    # Layk va Saqlanganlar
     likes = models.ManyToManyField(User, related_name='liked_stories', blank=True)
     saved_by = models.ManyToManyField(User, related_name='saved_stories', blank=True)
 
@@ -20,12 +24,11 @@ class Story(models.Model):
     views_count = models.IntegerField(default=0)
 
     class Meta:
-        ordering = ['-created_at']  # Eng yangisi tepada turadi
+        ordering = ['-created_at']
 
     def __str__(self):
         return self.title
 
-    # Vaqtni chiroyli ko'rsatish uchun (masalan: "2 hours ago")
     def get_date(self):
         return timesince(self.created_at) + " ago"
 
@@ -33,7 +36,13 @@ class Story(models.Model):
 # Rasm (Bitta hikoyada ko'p rasm bo'lishi mumkin)
 class StoryImage(models.Model):
     story = models.ForeignKey(Story, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='story_images/')
+
+    # 2. O'ZGARGAN JOY: storage=MediaCloudinaryStorage() qo'shildi
+    # Endi bu rasm ham to'g'ri Cloudinaryga yuklanadi
+    image = models.ImageField(
+        upload_to='story_images/',
+        storage=MediaCloudinaryStorage()
+    )
 
 
 # Kommentlar
